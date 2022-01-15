3fdba7e9b0794a84949ff112c409c2e8e178d81a
Antonio Hickey <antoniohickey99@gmail.com>
2022-01-15 13:19:06 -0500

    feat(basic learning): add route to store failed chars

--- app/python/routes/words.py
+++ app/python/routes/words.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from app.python.repositories import words as words_repo
 
 blueprint = Blueprint("words", __name__)
 
 
-@blueprint.route("/generate-words/<number>", methods=["GET"])
+@blueprint.route("/generate-words/<number>/")
 def generate_words(number: str):
-    return jsonify(words_repo.generate(int(number)))
+    return jsonify(words_repo.generate(
+        number_of_words=int(number),
+    ))
+
+
+@blueprint.route("/store-failure", methods=["POST"])
+def store_failure():
+    words_repo.fill_hashmap(
+        char=(request.data).decode("utf-8")[1],
+    )
+    return "200"
