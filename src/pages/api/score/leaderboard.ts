bddf1ff7b17a37f1d26331d7ec2ba1b7692bf6d8
Antonio Hickey <antoniohickey99@gmail.com>
2022-10-17 13:06:14 -0400

    refactor: clean up code

--- src/pages/api/score/leaderboard.ts
+++ src/pages/api/score/leaderboard.ts
@@ -8,14 +8,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    let test = await prisma.user.findMany({
+    let topTenUsers = await prisma.user.findMany({
       take: 10,
       orderBy: {
         highScore: "desc",
       },
     });
-    console.log(test);
-    res.status(200).json({ msg: "Grabbed top 10 users", users: test });
+    res.status(200).json({ msg: "Grabbed top 10 users", users: topTenUsers });
   } catch (error) {
     let message = "Unknown Error";
     if (error instanceof Error) message = error.message;
