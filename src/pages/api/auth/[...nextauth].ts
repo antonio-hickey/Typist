eaa716f36420fa9b845240741295d321b6480568
Antonio Hickey <antoniohickey99@gmail.com>
2022-10-17 12:41:13 -0400

    feat: setup prisma for type safe db abstraction

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prismadb";
 
 export const authOptions = {
   // Authentication Providers
@@ -9,6 +11,17 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  callbacks: {
+    // @ts-expect-error
+    async session({ session, user }) {
+      session.user.id = user.id;
+      session.user.highScore = user.highScore;
+      return session;
+    },
+  },
 };
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  ...authOptions,
+});
